f2bee12d5fe748cf7a47d914e3d223279aeb6754
msalehprofile <mollyhannia@hotmail.co.uk>
2024-10-01 10:42:30 +0100

    Created layout for events banner

---
+++ src/Component/EventSearchBanner/EventSearchBanner.tsx
@@ -0,0 +1,12 @@
+import './EventSearchBanner.scss';
+
+const EventSearchBanner = () => {
+  return (
+    <div className='event-search-banner'>
+        <h1 className='event-search-banner__heading'>Events Search</h1>
+        <p className='event-search-banner__summary'>Search for Events, Clubs, gig guide, Club Nights, Theatre and more. Buy your tickets or get on the guestlist for free</p>
+    </div>
+  )
+}
+
+export default EventSearchBanner
